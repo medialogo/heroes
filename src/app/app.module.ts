5e6396e64eda5897ce3f07dee102454a5fe6c3ef
t.ikemi (4230) <ikemi@medialogo.com>
2020-10-11 18:56:35 +0900

    done upto service injection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DragDropComponent } from './drag-drop/drag-drop.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     DragDropComponent,
+    HeroDetailComponent,
+    MessagesComponent,
   ],
   imports: [
     BrowserModule,
